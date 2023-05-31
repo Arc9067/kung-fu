cc6cbfaa95f97aebaa4124022b7116f6b1c57e54
Mikail Rokib <atandarokib@gmail.com>
2023-05-31 17:00:31 +0100

    updated

--- src/Components/How.jsx
+++ src/Components/How.jsx
@@ -8,10 +8,19 @@ const How = () => {
   return (
     <section className="w-full bg-primary py-[50px]" id="ape">
       <div className="container">
-        <h1 className="text-5xl md:text-7xl text-center shad2">how to ape </h1>
+        <h1
+          className="text-5xl md:text-7xl text-center shad2"
+          data-aos="fade-right"
+        >
+          how to ape{" "}
+        </h1>
 
         <div className="flex flex-wrap gap-10 w-full justify-between items-center mt-10">
-          <div className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center">
+          <div
+            className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center"
+            data-aos="fade-right"
+            data-aos-duration="500"
+          >
             <img src={Meta} className="w-[3rem]" alt="" />
             <div className="w-full ">
               <h1 className="text-3xl">CREATE A METAMASK WALLET</h1>
@@ -21,7 +30,11 @@ const How = () => {
               </p>
             </div>
           </div>
-          <div className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center">
+          <div
+            className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center"
+            data-aos="fade-left"
+            data-aos-duration="500"
+          >
             <img src={Eth} className="w-[3rem]" alt="" />
             <div className="w-full ">
               <h1 className="text-3xl">SEND ETH TO YOUR WALLET</h1>
@@ -31,7 +44,11 @@ const How = () => {
               </p>
             </div>
           </div>
-          <div className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center">
+          <div
+            className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center"
+            data-aos="fade-right"
+            data-aos-duration="500"
+          >
             <img src={Meta} className="w-[3rem]" alt="" />
             <div className="w-full ">
               <h1 className="text-3xl">CONNECT YOUR WALLET TO UNISWAP</h1>
@@ -40,7 +57,11 @@ const How = () => {
               </p>
             </div>
           </div>
-          <div className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center">
+          <div
+            className="min-h-[150px] gap-5 p-3 w-full lg:w-5/12 bg-primary/50 rounded-xl flex justify-between items-center"
+            data-aos="fade-left"
+            data-aos-duration="500"
+          >
             <img src={main} className="w-[3rem]" alt="" />
             <div className="w-full ">
               <h1 className="text-3xl">excahange</h1>
--- src/Components/Token.jsx
+++ src/Components/Token.jsx
@@ -6,10 +6,19 @@ const Token = () => {
   return (
     <section className="py-[50px] w-full bg-black">
       <div className="container">
-        <h1 className="text-5xl md:text-7xl text-center shad2">tokenomics </h1>
+        <h1
+          className="text-5xl md:text-7xl text-center shad2"
+          data-aos="fade-right"
+        >
+          tokenomics{" "}
+        </h1>
 
         <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-x-20 mt-16 gap-y-10">
-          <div className="h-[400px]  rounded-lg bg-primary/60 flex relative flex-col justify-between">
+          <div
+            className="h-[400px]  rounded-lg bg-primary/60 flex relative flex-col justify-between"
+            data-aos="fade-right"
+            data-aos-duration="500"
+          >
             <div className="">
               <img src={Logo3} alt="" />
             </div>
@@ -19,7 +28,11 @@ const Token = () => {
               </h1>
             </footer>
           </div>
-          <div className="h-[400px]  rounded-lg bg-primary/60 flex relative flex-col justify-between">
+          <div
+            className="h-[400px]  rounded-lg bg-primary/60 flex relative flex-col justify-between"
+            data-aos="fade-up"
+            data-aos-duration="500"
+          >
             <div className="">
               <img src={Logo4} alt="" />
             </div>
@@ -29,7 +42,11 @@ const Token = () => {
               </h1>
             </footer>
           </div>{" "}
-          <div className="h-[400px]  rounded-lg bg-black border ring-1 ring-primary flex relative flex-col justify-between">
+          <div
+            className="h-[400px]  rounded-lg bg-black border ring-1 ring-primary flex relative flex-col justify-between"
+            data-aos="fade-left"
+            data-aos-duration="500"
+          >
             <div className="">
               <img src={Logo3} alt="" />
             </div>
